64ddc2666a4b1a8946830ac8bde48f6255f88357
Marco Russo <badtrap@gmail.com>
2024-12-03 19:04:11 +0100

    feat: Precompile templates instead of dynamic loading

--- lib/servy/bear_controller.ex
+++ lib/servy/bear_controller.ex
@@ -1,28 +1,31 @@
 defmodule Servy.BearController do
   alias Servy.Wildthings
   alias Servy.Bear
+  alias Servy.BearView
 
-  @template_path Path.expand("../../templates", __DIR__)
+  #@template_path Path.expand("../../templates", __DIR__)
 
-  defp render(conv, template, bindings \\ []) do
-    content =
-    @template_path
-    |> Path.join(template)
-    |> EEx.eval_file(bindings)
-    %{ conv | status: 200, resp_body: content }
-  end
+  #defp render(conv, template, bindings \\ []) do
+  #  content =
+  #  @template_path
+  #  |> Path.join(template)
+  #  |> EEx.eval_file(bindings)
+  #  %{ conv | status: 200, resp_body: content }
+  #end
 
   def index(conv) do
     bears =
     Wildthings.list_bears()
     |> Enum.sort(&Bear.order_asc_by_name/2)
 
-    render(conv, "index.eex", bears: bears)
+    #render(conv, "index.eex", bears: bears)
+    %{ conv | status: 200, resp_body: BearView.index(bears) }
   end
 
   def show(conv, %{"id" => id}) do
     bear = Wildthings.get_bear(id)
-    render(conv, "show.eex", bear: bear)
+    #render(conv, "show.eex", bear: bear)
+    %{ conv | status: 200, resp_body: BearView.show(bear) }
   end
 
   def create(conv, %{"name" => name, "type" => type}) do
