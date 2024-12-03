14685febd496526c7442d25ebedd9f1250c3900d
Marco Russo <badtrap@gmail.com>
2024-12-03 17:18:41 +0100

    feat: Usr reduce instead of recursive fn to parse_headers

--- lib/servy/parser.ex
+++ lib/servy/parser.ex
@@ -9,7 +9,7 @@ defmodule Servy.Parser do
 
     [method, path, _] = String.split(request_line, " ")
 
-    headers = parse_headers(header_lines, %{})
+    headers = parse_headers(header_lines)
     params = parse_params(headers["Content-Type"], params_string)
 
     IO.inspect header_lines
@@ -22,13 +22,13 @@ defmodule Servy.Parser do
       }
   end
 
-  def parse_headers([head | tail], headers) do
-    [key, value] = String.split(head, ": ")
-    headers = Map.put(headers, key, value)
-    parse_headers(tail, headers)
+  def parse_headers(header_lines) do
+    header_lines
+    |> Enum.reduce(%{}, fn line, acc ->
+      [key, value] = String.split(line, ": ")
+      Map.put(acc, key, value)
+    end)
   end
-
-  def parse_headers([], headers), do: headers
 
   def parse_params("application/x-www-form-urlencoded", params_string) do
     IO.puts "+++++++++++ parse_params matched ++++++++++++++"
